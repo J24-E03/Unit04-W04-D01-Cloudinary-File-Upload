5bfa9742b882b7f822944ccdb9a79f8010def058
omarakamal <omaraakamal@gmail.com>
2025-05-19 10:33:56 +0300

    update

---
+++ Class-Code/Cloudinary-Upload/src/main/java/com/dci/full_mvc/model/ImageMetaData.java
@@ -0,0 +1,29 @@
+package com.dci.full_mvc.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ImageMetaData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long imageId;
+
+    private String path;
+
+    private String publicId;
+
+
+}
